7fe2174ab5e5833534b0e59e425f64222edea446
Roman Bekkiev <rbekkiev@inv485.mwm.local>
2015-09-20 17:33:46 +0300

    fix html plugin

--- webpack/config-main.js
+++ webpack/config-main.js
@@ -1,34 +1,40 @@
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlPlugin = require('html-webpack-plugin');
 
 var REGEX = {
-		JS: /\.js/,
-		SSCC: /\.scss/,
-		NODE_MODULES: /(node_modules)/
+	JS: /\.js/,
+	SSCC: /\.scss/,
+	NODE_MODULES: /(node_modules)/
 };
 
-var SOURCE_DIR = require('path').resolve(__dirname, "..", "src");
-var RELEASE_DIR = require('path').resolve(__dirname, "..", "public");
+var SOURCE_DIR = require('path').resolve(__dirname, '..', 'src');
+var RELEASE_DIR = require('path').resolve(__dirname, '..', 'public');
 var dependencies  = Object.keys(require('../package.json').dependencies);
 
 module.exports = {
 		module: {
-				loaders: [
-						{test: REGEX.JS,   loader: 'babel', exclude: REGEX.NODE_MODULES},
-						{test: REGEX.SSCC, loader: ExtractTextPlugin.extract('css!autoprefixer-loader?browsers=last 3 version!sass')}
-				]
+			loaders: [
+				{test: REGEX.JS,   loader: 'babel', exclude: REGEX.NODE_MODULES},
+				{test: REGEX.SSCC, loader: ExtractTextPlugin.extract('css!autoprefixer-loader?browsers=last 3 version!sass')}
+			]
 		},
 		plugins: [
-				new webpack.optimize.CommonsChunkPlugin({name: 'vendors', filename: './js/vendors.js'}),
-				new ExtractTextPlugin('css/style.css'), // extracts css and put it to <output.path>/css/style.css
+			new webpack.optimize.CommonsChunkPlugin({name: 'vendors', filename: 'js/vendors.js'}),
+			new ExtractTextPlugin('css/style.css'), // extracts css and put it to <output.path>/css/style.css
+			new HtmlPlugin({
+				title: 'Test APP',
+				filename: 'index.html'
+			})
 		],
 		context: SOURCE_DIR,
 		entry: { // entry points - files that contains 'require' and will be concat into bundle
-				app: "./scripts/index.js", // main enty point
-				vendors: dependencies // entry points for CommonsChunkPlugin
+			app: './1scripts/index.js', // main enty point
+			vendors: dependencies // entry points for CommonsChunkPlugin
 		},
 		output: {
-				path: RELEASE_DIR,
-				filename: "./js/app.js"
+			path: RELEASE_DIR,
+			filename: 'js/app.js',
+			publicPath: '/'
 		}
 };
