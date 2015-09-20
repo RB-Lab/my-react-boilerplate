4959d24e4126af59f862e1a5763307a2741f48dd
Roman Bekkiev <rbekkiev@inv485.mwm.local>
2015-09-20 17:15:07 +0300

    optimize console output for dev-server

--- webpack/dev-config.js
+++ webpack/dev-config.js
@@ -2,7 +2,20 @@ var mainConf = require('./config-main');
 var merge = require('lodash').merge.bind(null, {});
 
 module.exports = merge(mainConf, {
-    devtool: 'inline-source-map',
-    cache: true,
-    stats: {colors: true, reasons: true}
+	devtool: 'inline-source-map',
+	cache: true,
+	debug: false,
+	stats: {
+		assets: true,
+		colors: true,
+		version: true,
+		modules: false,
+		hash: false,
+		timings: false,
+		chunks: true,
+		chunkModules: false,
+		reasons : true,
+		cached : true,
+		chunkOrigins : true
+	}
 });
--- webpack/serve.js
+++ webpack/serve.js
@@ -3,15 +3,16 @@ var WebpackDevServer = require('webpack-dev-server');
 var config = require('./dev-config');
 var argv = require('minimist')(process.argv.slice(2));
 
-var port = argv.port || argv.p || 3000
+var port = argv.port || argv.p || 3000;
 
 new WebpackDevServer(webpack(config), {
-    contentBase: 'src/',
-    publicPath: config.output.publicPath,
-    historyApiFallback: true
+	contentBase: 'src/',
+	publicPath: config.output.publicPath,
+	historyApiFallback: true,
+	stats: config.stats
 }).listen(port, '127.0.0.1', function (err) {
-    if (err) {
-        return console.log(err);
-    }
-    console.log('Listening at 127.0.0.1:' + port);
+	if (err) {
+		return console.log(err);
+	}
+	console.log('Listening at 127.0.0.1:' + port);
 });
