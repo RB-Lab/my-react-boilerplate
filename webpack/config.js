0aa0a7a18c59f4b8a646532dcfb927e61161f7ad
Roman Bekkiev <rbekkiev@inv485.mwm.local>
2015-09-23 19:05:04 +0300

    fix html template etc

--- webpack/config.js
+++ webpack/config.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlPlugin = require('html-webpack-plugin');
+var argv = require('minimist')(process.argv.slice(2));
 
 var REGEX = {
 	JS: /\.js/,
@@ -11,6 +12,10 @@ var REGEX = {
 var SOURCE_DIR = require('path').resolve(__dirname, '..', 'src');
 var RELEASE_DIR = require('path').resolve(__dirname, '..', 'public');
 var dependencies  = Object.keys(require('../package.json').dependencies);
+
+
+var DEBUG = !!(argv.debug || argv.d);
+var VERBOSE = !!(argv.verbose || argv.v);
 
 module.exports = {
 	context: SOURCE_DIR,
@@ -33,22 +38,21 @@ module.exports = {
 	plugins: [
 		new webpack.optimize.CommonsChunkPlugin({name: 'vendors', filename: 'js/vendors.js'}),
 		new ExtractTextPlugin('css/style.css'), // extracts css and put it to <output.path>/css/style.css
-		new HtmlPlugin({ // gets the index.html from `context` dir as a template and create from it <output.path>/index.html
+		new HtmlPlugin({ // gets the index.html from SOURCE_DIR as a template and create from it <output.path>/index.html
+			template: SOURCE_DIR + '/index.html',
 			title: 'Test APP',
 			filename: 'index.html'
 		})
 	],
 	stats: { // The stats actually used in build and dev-server scripts
-		assets: true,
 		colors: true,
-		version: true,
-		modules: false,
-		hash: false,
-		timings: false,
-		chunks: true,
-		chunkModules: false,
-		reasons : true,
-		cached : true,
-		chunkOrigins : true
+		reasons: DEBUG,
+		hash: VERBOSE,
+		version: VERBOSE,
+		timings: VERBOSE,
+		chunks: VERBOSE,
+		chunkModules: VERBOSE,
+		cached: VERBOSE,
+		cachedAssets: VERBOSE
 	}
 };
