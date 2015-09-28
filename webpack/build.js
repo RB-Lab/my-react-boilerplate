49b1ee1544af692f8d6cb4771c029ad89efe756f
Roman Bekkiev <rbekkiev@inv485.mwm.local>
2015-09-28 20:07:50 +0300

    yet another small fixes

--- webpack/build.js
+++ webpack/build.js
@@ -1,6 +1,5 @@
 var webpack = require('webpack');
-
-var config = argv.production ? require('./config.prod') : require('./config.dev');
+var config = require('./config.build');
 
 webpack(config).run(function(err, stats){
 	console.log(stats.toString(config.stats));
--- webpack/config.build.js
+++ webpack/config.build.js
@@ -1,6 +1,6 @@
 var webpack = require('webpack');
 var mainConf = require('./config');
-var merge = require('lodash').merge.bind(null, {});
+var merge = require('lodash').merge;
 var argv = require('minimist')(process.argv.slice(2));
 
 var PRODUCTION_MODE = !!argv.production;
@@ -12,7 +12,7 @@ var prodPlugins = [
 	new webpack.optimize.AggressiveMergingPlugin()
 ];
 
-module.exports = mergeConf(baseConf, {
+module.exports = merge({}, baseConf, {
 	devtool: 'source-map',
 	plugins: baseConf.plugins.concat(PRODUCTION_MODE ? buildPlugins.concat(prodPlugins) : buildPlugins)
 });
